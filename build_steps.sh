06efb7e8f4c1b04d7c1b566fdbb29e07107c01b3
Donghoon Park <donghun94@gmail.com>
2023-07-16 11:21:09 +0900

    Update build_steps.sh

--- build_steps.sh
+++ build_steps.sh
@@ -1,10 +1,11 @@
-# Build step of antimicro in ubuntu 18.04 bionic aarch64(64bit arm)
+# Build step of antimicro in ubuntu 18.04/20.04 aarch64(64bit arm)
 # https://github.com/AntiMicroX/antimicrox/blob/master/BUILDING.md
 
 # setup dependencies
 sudo apt install g++ cmake extra-cmake-modules qttools5-dev qttools5-dev-tools libsdl2-dev libxi-dev libxtst-dev libx11-dev itstool gettext ninja-build
 
 # upgrade cmake version to 3.25
+# Note: Only mandatory for 18.04 bionic
 sudo apt remove --purge --auto-remove cmake
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
